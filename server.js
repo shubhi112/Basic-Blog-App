71d2130a97b2309c81c17ad049ebd2ce48259a9d
shubhi112 <49321297+shubhi112@users.noreply.github.com>
2021-10-27 21:40:46 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Article = require('./models/article')
+const articleRouter = require("./routes/articles");
+const methodOverride = require("method-override")
+const app = express();
+
+mongoose.connect("mongodb://localhost/blog", { useNewUrlParser: true });
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false}))
+ 
+app.use(methodOverride('_method'))
+
+app.use('/public', express.static('public'));
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
+    res.render('articles/index', { articles: articles })
+  })
+
+app.use("/articles", articleRouter);
+ 
+app.listen(4000);
